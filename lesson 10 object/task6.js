86aac616fd3c69709b95be823b4c9be2284d0645
mazanyCech <cyberpunk1408@gmail.com>
2022-11-28 22:56:13 +0300

    stage 1

--- lesson 10 object/task6.js
+++ lesson 10 object/task6.js
@@ -1,1 +1,51 @@
-console.log();
+const heroPlayer ={
+    name: "Timbersaw",
+    health: 100,
+    heatEnemy(enemyPlayer){
+        let decreaseHealthEnemy = enemyPlayer.health - 10;
+        //console.log(decreaseHealthEnemy);
+        
+    }
+};
+
+
+const enemyPlayer ={
+    name: "Bane",
+    health: 100,
+    heatHero(heroPlayer){
+        heroPlayer.heatEnemy(this.health); 
+        heroPlayer.health - 10;
+         
+    }
+    
+}
+
+
+//Function start game where the players will fight each other 
+//if ti`ll be 0 then calls method "heatEnemy", if it`ll be 1 then calls method "heatHero"
+function startGame(heroPlayer, enemyPlayer, resultRandomNumber){
+    heroPlayer.heatEnemy(enemyPlayer);
+    enemyPlayer.heatHero(heroPlayer);
+
+    
+    
+    while (heroPlayer.heatEnemy(enemyPlayer) >= 0 ){
+        if(resultRandomNumber == 0){
+            heroPlayer.heatEnemy(enemyPlayer);
+            //console.log(heroPlayer.heatEnemy(enemyPlayer));
+            console.log(enemyPlayer);
+        } 
+    }
+    
+}
+
+//Function get random number for function startGame
+function getRandomNumberInRange(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+let resultRandomNumber = getRandomNumberInRange(0 , 1);
+
+startGame(heroPlayer, enemyPlayer, resultRandomNumber);
