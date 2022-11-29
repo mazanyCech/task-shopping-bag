8e98e6489d5d3d08a66b7770e50c48ecbeed6100
mazanyCech <cyberpunk1408@gmail.com>
2022-11-29 23:31:29 +0300

    finished the task

--- lesson 10 object/task6.js
+++ lesson 10 object/task6.js
@@ -1,10 +1,8 @@
 const heroPlayer ={
     name: "Timbersaw",
     health: 100,
-    heatEnemy(enemyPlayer){
-        let decreaseHealthEnemy = enemyPlayer.health - 10;
-        //console.log(decreaseHealthEnemy);
-        
+    heatEnemy(enemyPlayer){        
+        console.log(enemyPlayer.health = enemyPlayer.health - 10);
     }
 };
 
@@ -13,29 +11,33 @@ const enemyPlayer ={
     name: "Bane",
     health: 100,
     heatHero(heroPlayer){
-        heroPlayer.heatEnemy(this.health); 
-        heroPlayer.health - 10;
-         
+        console.log(heroPlayer.health = heroPlayer.health - 10);
     }
-    
 }
+
 
 
 //Function start game where the players will fight each other 
 //if ti`ll be 0 then calls method "heatEnemy", if it`ll be 1 then calls method "heatHero"
-function startGame(heroPlayer, enemyPlayer, resultRandomNumber){
-    heroPlayer.heatEnemy(enemyPlayer);
-    enemyPlayer.heatHero(heroPlayer);
+function startGame(heroPlayer, enemyPlayer, getRandomNumberInRange){
+ 
+    while (enemyPlayer.health >= 0 && heroPlayer.health >= 0){      
+        let random = getRandomNumberInRange(0, 1);
+        if(random === 0){
+            heroPlayer.heatEnemy(enemyPlayer);           
+            
+        } else if(random === 1){
+            enemyPlayer.heatHero(heroPlayer);
+                    
+        }
 
-    
-    
-    while (heroPlayer.heatEnemy(enemyPlayer) >= 0 ){
-        if(resultRandomNumber == 0){
-            heroPlayer.heatEnemy(enemyPlayer);
-            //console.log(heroPlayer.heatEnemy(enemyPlayer));
-            console.log(enemyPlayer);
-        } 
     }
+    if(enemyPlayer.health > 0 ){
+        alert(`${enemyPlayer.name} won, He had ${enemyPlayer.health} health points`);
+    } else if(heroPlayer.health > 0){
+        alert(`${heroPlayer.name} won, He had ${heroPlayer.health} health points`);
+    }
+    
     
 }
 
@@ -44,8 +46,6 @@ function getRandomNumberInRange(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+//let result = getRandomNumberInRange(0, 1);
 
-
-let resultRandomNumber = getRandomNumberInRange(0 , 1);
-
-startGame(heroPlayer, enemyPlayer, resultRandomNumber);
+startGame(heroPlayer, enemyPlayer, getRandomNumberInRange);
