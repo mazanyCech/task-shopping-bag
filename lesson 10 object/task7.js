bf75a281db779668b85b8069c621cc0e0527ae41
mazanyCech <cyberpunk1408@gmail.com>
2022-12-03 21:59:45 +0300

    completed the task

--- lesson 10 object/task7.js
+++ lesson 10 object/task7.js
@@ -6,11 +6,23 @@ const suspectInfo = {
 
 const deadPeople = ["Lucas", "Bill"];
 
+
 function getKiller(suspectInfo, deadPeople){
-    
-    for (let name of Object.keys(suspectInfo)) {
+    let count = 0;
+    for (let value in suspectInfo) {
+        for(let i = 0; i < suspectInfo[value].length; i++){          
+           for(let j = 0; j < deadPeople.length; j++){
+                if(suspectInfo[value][i] === deadPeople[j]){
+                    count++;
+                    if(count > 1 && count < 3){
+                        console.log(value);
+                    }
+
+                }
+            }
+        }
+
         
-        console.log(name);
     }
 }
 
